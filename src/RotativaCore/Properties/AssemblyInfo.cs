198fbf4f554071cfc73138572e019e45494a684c
arichika taniguchi <email@arichika.net>
2017-12-20 17:49:27 +0900

    update nuspec by csproj
    update nuspec by csproj

--- src/RotativaCore/Properties/AssemblyInfo.cs
+++ src/RotativaCore/Properties/AssemblyInfo.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RotativaCore")]
-[assembly: AssemblyDescription("Print Pdf from ASP.NET Core")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RotativaCore")]
